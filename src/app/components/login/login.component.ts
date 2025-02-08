add5322c28e5669dbe3d2c6c979231cc52d5dc41
Daniel Moraes Mariz <danielmrz.dev@gmail.com>
2025-02-08 19:13:35 -0300

    Cria páginas Login e Create an Account

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  @Input({ required: true }) isLoginPage: boolean = true;
 }
