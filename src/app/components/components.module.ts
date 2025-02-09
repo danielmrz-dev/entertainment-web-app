9ed14f8f6b5a6cd44ac36560c55f642091d19fac
Daniel Moraes Mariz <danielmrz.dev@gmail.com>
2025-02-08 23:23:46 -0300

    Cria componente de home page

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from '../app-routing.module';
 import { LoginAndRegisterFormComponent } from './login-and-register-form/login-and-register-form.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    LoginAndRegisterFormComponent
+    LoginAndRegisterFormComponent,
+    HomePageComponent,
   ],
   imports: [
     CommonModule,
